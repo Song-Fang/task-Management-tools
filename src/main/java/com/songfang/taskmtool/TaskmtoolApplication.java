a6faee06c4e9ead1608526ac063b3b5e405b8657
Song-Fang <sof10@pitt.edu>
2020-02-26 01:50:02 -0500

    first commit

---
+++ src/main/java/com/songfang/taskmtool/TaskmtoolApplication.java
@@ -0,0 +1,13 @@
+package com.songfang.taskmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmtoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songfang/taskmtool/TaskmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songfang.taskmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
