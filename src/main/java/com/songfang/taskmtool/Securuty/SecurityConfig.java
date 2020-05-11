bf62b8f4a949aea0b743530b1ab8536b9ed30f6c
Song-Fang <sof10@pitt.edu>
2020-05-10 20:15:04 -0400

    Signed-off-by: Song-Fang <sof10@pitt.edu>

--- src/main/java/com/songfang/taskmtool/Securuty/SecurityConfig.java
+++ src/main/java/com/songfang/taskmtool/Securuty/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.songfang.taskmtool.Securuty;
 
 import com.songfang.taskmtool.Services.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static com.songfang.taskmtool.Securuty.SecurityConstraints.H2_URL;
 import static com.songfang.taskmtool.Securuty.SecurityConstraints.SIGN_UP_URL;
@@ -29,12 +33,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailService customUserDetailService;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailService);
+        auth.userDetailsService(customUserDetailService).passwordEncoder(bCryptPasswordEncoder);
     }
 
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
