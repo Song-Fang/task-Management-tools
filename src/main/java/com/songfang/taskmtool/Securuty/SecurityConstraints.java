6b5ab4aa0bf0d561c9f4af6e1eecb3e022738706
Song-Fang <sof10@pitt.edu>
2020-05-10 20:26:20 -0400

    Signed-off-by: Song-Fang <sof10@pitt.edu>

--- src/main/java/com/songfang/taskmtool/Securuty/SecurityConstraints.java
+++ src/main/java/com/songfang/taskmtool/Securuty/SecurityConstraints.java
@@ -5,5 +5,6 @@ public class SecurityConstraints {
     public static final String H2_URL = "h2-console/**";
     public static final String TOKEN_PREFIX = "Bearer";
     public static final String HEAD_STRING = "Authorization";
+    public static final String SECRET = "SecretKeyToGenJWTs";
     public static final long EXPIRATION_TIME=30_000;
 }
